bd7718cb13c68134b87e489e104237068d308dca
Ludougan123234 <randy19981223@gmail.com>
2021-09-20 14:30:06 +0800

    Add comments and change code formatting

--- Research R Code.R
+++ Research R Code.R
@@ -2,9 +2,9 @@
 getwd()
 setwd("#MY/WORKING/DIRECTORY#")
 
-##### Install necessari packages#####
+##### Install necessary packages#####
 packages <- c("ltm", "lavaan", "tidySEM", "psych", "pastecs", "ggplot2", "ggpubr", "GGally", "rstatix", "corrplot", "PerframnceAnalytics",
-              "Hmisc", "nFactors", "paran", "dplyr", "DAAG")
+              "Hmisc", "nFactors", "paran", "dplyr", "DAAG", "ggiraphExtra")
 installed_packages <- packages %in% rownames(installed.packages())
 if(any(installed_packages = FALSE)){
   install.packages(packages[!installed_packages])
@@ -20,30 +20,34 @@ ars_rev_key <- c(1,-1,1,-1,1,-1,1,-1,1,1,1,1,1,1,1,1,-1) #2,4,6,8,17 reverse cod
 #raw responses
 demographic <- surv_dat[, c(1:2)]
 flcas_rep <- surv_dat[, c(3:22)]
-ars_rep <- reverse.code(ars_rev_key, surv_dat[, c(23:39)]) #reverse.code(key, data)
+ars_rep <- reverse.code(ars_rev_key, surv_dat[, c(23:39)]) #reverse.code(key, data) to reverse code ARS items
 btps_rep <- surv_dat[, c(40:52)]
 
 #####Cronbach's alpha#####
+# calculate cronbach's alpha of the three questionnaires
 cronbach.alpha(flcas_rep) # = 0.955
 cronbach.alpha(ars_rep)  # = 0.876
 cronbach.alpha(btps_rep) # = 0.827
 
 #####factor analysis prep#####
+# Preparation of factor analysis - calculate KMO index of sampling adequacy, bartlett's sphericity
 KMO(ars_rep)
 KMO(btps_rep)
 KMO(flcas_rep)
-factanal(btps_rep, factors = 2) #could be 3 as well
-factanal(ars_rep, factors = 3)
-factanal(flcas_rep, factors = 3)
+antimg(ars_rep)
+antimg(btps_rep)
+antimg(flcas_rep)
+
 
 #####factor analysis flcas#####
+# all three questionnaires go through eigenvalue calculation and parallel analysis to determine the number of factors to be retained, 
 ev <- eigen(cor(flcas_rep))
 ap <- parallel(subject = nrow(flcas_rep), var = ncol(flcas_rep), rep = 100, cent = 0.05)
 ns <- nScree(x = ev$values, aparallel = ap$eigen$qevpea)
 paran(flcas_rep, iterations = 5000, centile = 0, quietly = FALSE, 
       status = TRUE, all = TRUE, cfa = TRUE, graph = TRUE, color = TRUE, 
       col = c("black", "red", "blue"), lty = c(1, 2, 3), lwd = 1, legend = TRUE, 
-      file = "", width = 640, height = 640, grdevice = "png", seed = 0)
+      file = "", width = 640, height = 640, grdevice = "png", seed = 0) +Ttitle("FLCAS")
 plotnScree(ns)
 
 factanal(flcas_rep, factors = 3)
@@ -56,7 +60,7 @@ plotnScree(ns)
 paran(ars_rep, iterations = 5000, centile = 0, quietly = FALSE, 
       status = TRUE, all = TRUE, cfa = TRUE, graph = TRUE, color = TRUE, 
       col = c("black", "red", "blue"), lty = c(1, 2, 3), lwd = 1, legend = TRUE, 
-      file = "", width = 640, height = 640, grdevice = "png", seed = 0) + title("ARS")
+      file = "", width = 640, height = 640, grdevice = "png", seed = 0) + title("ARS-30")
 factanal(ars_rep, factors = 3)
 fa(ars_rep, nfactors = 3, rotate = "oblimin")
 
@@ -68,12 +72,16 @@ plotnScree(ns)
 paran(btps_rep, iterations = 5000, centile = 0, quietly = FALSE, 
       status = TRUE, all = TRUE, cfa = TRUE, graph = TRUE, color = TRUE, 
       col = c("black", "red", "blue"), lty = c(1, 2, 3), lwd = 1, legend = TRUE, 
-      file = "", width = 640, height = 640, grdevice = "png", seed = 0)
+      file = "", width = 640, height = 640, grdevice = "png", seed = 0) + title("BTPS")
 factanal(btps_rep, factors = 3)
 fa(btps_rep, nfactors = 3, rotate = "oblimin")
 
+factanal(btps_rep, factors = 2) 
+factanal(ars_rep, factors = 3)
+factanal(flcas_rep, factors = 3)
 
-#factors
+
+# binding factors into their respective dataframes with rowSums()
 flcas.com_app.low_est <- surv_dat[, c(3,4,5,6,8,9,11,12,13,14,17,18,20,21,22)]
 flcas.com_app.low_est.total <- rowSums(flcas.com_app.low_est)
 flcas.neg_att <- surv_dat[, c(7,10,15,16,19)]
@@ -156,7 +164,8 @@ chart.Correlation(total_scores)
 cor.plot(total_scores)
 #ggpairs(total_scores)
 
-
+# plot correlation plots by group
+# use grab_legend() to grab legend from a different ggplot() plot to yield more readable legend
 legend_age <- grab_legend(ggplot(total_scores, aes(x = "flcas_total", y = "btps_total", color = demographic[, 2])) + geom_point() + scale_color_discrete("Age groups"))
 ggpairs(total_scores, 
         ggplot2::aes_string(colour = demographic[, 2]), 
@@ -187,7 +196,7 @@ ggpairs(factored_total,
         legend = legend_age) + theme(legend.position = "bottom", legend.)
 
 #####CFL and structural regression#####
-flcas_factor <- "CA_LSC =~ FLCAS_1 + FLCAS_2 + FLCAS_3 + FLCAS_4+ FLCAS_6+ FLCAS_7+ FLCAS_9+ FLCAS_10+ FLCAS_11++ FLCAS_12+ FLCAS_15+ FLCAS_16+ FLCAS_18+ FLCAS_19+ FLCAS_20
+flcas_factor <- "CA_LSC =~ FLCAS_1 + FLCAS_2 + FLCAS_3 + FLCAS_4+ FLCAS_6+ FLCAS_7+ FLCAS_9+ FLCAS_10+ FLCAS_11 + FLCAS_12+ FLCAS_15+ FLCAS_16+ FLCAS_18+ FLCAS_19+ FLCAS_20
 NATEC =~ FLCAS_5+ FLCAS_8+ FLCAS_13+ FLCAS_14+ FLCAS_17"
 flcas_cfa <- cfa(flcas_factor, data = flcas_rep)
 summary(flcas_cfa, fit.measures = TRUE, standardized = TRUE)
@@ -272,8 +281,7 @@ cor_mark_significant(flcas_cor)
 cor_mat(btps_rep)
 cor_mat(ars_rep)
 
-install.packages("ggiraphExtra")
-library(ggiraphExtra)
+
 
 
 ######plot function#####
